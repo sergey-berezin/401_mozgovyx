d27b7410595feed8524312a2459c4942b3168bf9
vasily-mozgovykh <s02170168@gse.cs.msu.ru>
2023-09-21 23:32:18 +0300

    Added forward pass asynchronous logic

--- Lab 1/YoloPackage/YoloPackage.cs
+++ Lab 1/YoloPackage/YoloPackage.cs
@@ -1,9 +1,26 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Drawing.Processing;
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
 
 namespace YoloPackage
 {
     public class YoloService
     {
-        // TODO content
+        private const string ModelURL = "https://storage.yandexcloud.net/dotnet4/tinyyolov2-8.onnx";
+        private InferenceSession? Session = null;
+        private SemaphoreSlim SessionLock = new SemaphoreSlim(1, 1);
+
+        private async Task<IDisposableReadOnlyCollection<DisposableNamedOnnxValue>> Forward(List<NamedOnnxValue> inputs) {
+            await SessionLock.WaitAsync();
+            var outputs = Session.Run(inputs);
+            SessionLock.Release();
+            return outputs;
+        }
     }
 }
