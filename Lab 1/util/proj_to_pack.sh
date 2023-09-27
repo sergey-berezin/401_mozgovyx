e8bd6fbd1cb6454b5ff375bcaad65cf3042dd617
vasily-mozgovykh <s02170168@gse.cs.msu.ru>
2023-09-28 01:29:34 +0300

    Changed package versions

--- Lab 1/util/proj_to_pack.sh
+++ Lab 1/util/proj_to_pack.sh
@@ -1,3 +1,3 @@
 dotnet sln remove YoloPackage
 dotnet remove ConsoleApp reference ./YoloPackage/YoloPackage.csproj
-dotnet add ConsoleApp package Mozgovyx.YoloPackage --version 0.1.2309.2
+dotnet add ConsoleApp package Mozgovyx.YoloPackage --version 2.1.2709.1
