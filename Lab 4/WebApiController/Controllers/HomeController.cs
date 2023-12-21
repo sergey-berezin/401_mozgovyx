12b6d56589354853d95592fc3e2e8ef9004c84ae
vasily-mozgovykh <s02170168@gse.cs.msu.ru>
2023-12-21 20:03:12 +0300

    Added WebAPI controller

---
+++ Lab 4/WebApiController/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp;
+using YoloPackage;
+
+
+namespace WebApiController.Controllers
+{
+    [Route("api/segmentation")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+        public HomeController() { }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] string strImage)
+        {
+            if (strImage == null || strImage.Length == 0)
+            {
+                return BadRequest("Controller got an empty string");
+            }
+            try
+            {
+                byte[] bytePixels = Convert.FromBase64String(strImage);
+                Image<Rgb24> image;
+                using (MemoryStream ms = new MemoryStream(bytePixels))
+                {
+                    image = Image.Load<Rgb24>(ms);
+                }
+                var cts = new CancellationTokenSource();
+                var segmentation = await YoloService.ProcessImage(image, cts.Token);
+                var result = segmentation.BoundingBoxes.Select(x =>
+                    new ImagePresentation(
+                        ProcessingTools.CutBoundingBox(image, x),
+                        YoloService.Labels[x.Class],
+                        x.Confidence
+                    )
+                ).ToList();
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
---
+++ Lab 4/WebApiController/ImagePresentation.cs
@@ -0,0 +1,21 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using System.Runtime.CompilerServices;
+
+namespace WebApiController
+{
+    public class ImagePresentation
+    {
+        public byte[] Image { get; set; }
+        public string ClassName { get; set; }
+        public double Confidence { get; set; }
+
+        public ImagePresentation(Image<Rgb24> image, string className, double confidence)
+        {
+            Image = new byte[image.Width * image.Height * Unsafe.SizeOf<Rgb24>()];
+            image.CopyPixelDataTo(Image);
+            ClassName = className;
+            Confidence = confidence;
+        }
+    }
+}
